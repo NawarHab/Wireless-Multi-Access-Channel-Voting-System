97d87b2a424083954b7ea6a51370d52df87925da
Silver Kuusik <silver.kuusik@gmail.com>
2012-07-26 22:44:27 +0200

    cleaned up compiled stuff

--- voteino_server/voteino/settings.py
+++ voteino_server/voteino/settings.py
@@ -1,5 +1,9 @@
 # Django settings for voteino project.
 
+import os, sys
+# get the project root path, add .. because this file is not in root folder
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))+'/../'
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 DAJAXICE_DEBUG = DEBUG
@@ -28,7 +32,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = None
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -49,18 +53,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/silbo/itk/Python/media/'
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/silbo/itk/Python/static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -71,7 +75,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/silbo/itk/Python/voteino/polls/',
 )
 
 # List of finder classes that know how to find static files in
@@ -119,7 +122,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/silbo/itk/Python/voteino/templates',
+    PROJECT_ROOT+'templates',
 )
 
 INSTALLED_APPS = (
