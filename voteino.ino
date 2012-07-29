df38b46ea4e9ac06176b815aa75091b4f5ee6aab
Silver <silver.kuusik@gmail.com>
2012-07-29 16:14:10 +0300

    Update voteino.ino

--- voteino.ino
+++ voteino.ino
@@ -1,25 +1,23 @@
 /*
- *  Voteino by Silver Kuusik ( Multiuser Voting system for Arduino )
- *  Copyright (C) 2012 Silver Kuusik
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *  Author (C) 2012 Silver Kuusik <silver.kuusik@gmail.com>
- *  Contributor (C) 2012 Balachandar Vittal <bala561989@gmail.com>
- *  Credits to https://github.com/maniacbug/RF24 developers, great project
- */
+Voteino - Multiuser Voting system for Arduino
+
+Copyright (C) 2012 Silver Kuusik <silver.kuusik@gmail.com>
+Copyright (C) 2012 Balachandar Vittal <561989@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #include <SPI.h>
 #include "nRF24L01.h"
